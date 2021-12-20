188538ef2083c6493db4ca44839d687ad7b554d1
Nandhini <53011385+Nandhinianandh@users.noreply.github.com>
2021-12-20 18:37:19 +0530

    Wrapper

---
+++ Wrapper/Demo.java
@@ -0,0 +1,9 @@
+public class Demo {
+    public static void main(String [] args)
+    {
+        Integer number=new Integer(10);
+        Integer num=10;
+
+        int primitiveNum=number;
+    }
+}
