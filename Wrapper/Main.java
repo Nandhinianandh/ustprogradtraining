db5b86e52f79f31df088f191f84db4a3aef035c8
Nandhini <53011385+Nandhinianandh@users.noreply.github.com>
2021-12-20 18:35:01 +0530

    Wrapper

---
+++ Wrapper/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Integer number=new Integer(10);
+
+       int a=number.intValue();//unboxing
+
+        Integer num=Integer.valueOf(a);//autoboxing
+
+        Integer age=18;
+        int primitiveAge=age;
+    }
+}
