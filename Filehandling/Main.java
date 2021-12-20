7b0f55a41347d51e80e1c52f4b16f0ddaaa1986c
Nandhini <53011385+Nandhinianandh@users.noreply.github.com>
2021-12-20 18:19:53 +0530

    Filehandling

---
+++ Filehandling/Main.java
@@ -0,0 +1,19 @@
+import java.io.*;
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        File file = new File("programs/c.txt");
+       // file.createNewFile();
+       System.out.println(file.canRead());
+//        System.out.println(file.exists());
+//        System.out.println(file.delete());
+//        System.out.println(file.mkdir());
+//        String[] files=file.list(); //[file1 , file2]
+//        for(int  i=0;i<files.length;i++){
+//            System.out.println(files[i]);
+//        }
+//        System.out.println(file.length());
+//        System.out.println(file.delete());
+//    }
+    }
+}
