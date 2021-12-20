72e2c41b512b0994cd1512af1a34ee3add5f1120
Nandhini <53011385+Nandhinianandh@users.noreply.github.com>
2021-12-20 18:02:43 +0530

    Day2

---
+++ Day2/Playing with cubes I.java
@@ -0,0 +1,15 @@
+public class Cube
+{
+private int Side;
+
+  // Get method
+  public int getSide() {
+    return Side;
+  }
+  
+  //set method
+  public void setSide(int num){
+  this.Side = num;
+
+}
+  }
