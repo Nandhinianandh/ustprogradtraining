554387194d6eca8b49cecc561c51b14b30aba9e8
Nandhini <53011385+Nandhinianandh@users.noreply.github.com>
2021-12-20 18:52:55 +0530

    tdd

---
+++ TDD/Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+    public double addition(double a,double b){
+        return a+b;
+    }
+    public double subtraction(double a,double b)
+    {
+        return a-b;
+    }
+    public double multiplication(double a,double b)
+    {
+        return a*b;
+    }
+    public double division(double a,double b){
+        if(b==0){
+            throw new ArithmeticException("cannot divide by zero");
+        }
+        return a/b;
+    }
+}
