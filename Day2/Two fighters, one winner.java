afdbb2eed164db53dd9c82acc4902630e4815401
Nandhini <53011385+Nandhinianandh@users.noreply.github.com>
2021-12-20 18:03:33 +0530

    Day2

---
+++ Day2/Two fighters, one winner.java
@@ -0,0 +1,14 @@
+public class Kata {
+  public static String declareWinner(Fighter fighter1, Fighter fighter2, String firstAttacker) {
+    // Your code goes here. Have fun!
+    Fighter a=fighter1, b=fighter2;
+    if (firstAttacker.equals(fighter2.name)) {
+      a = fighter2; b = fighter1;
+    }    
+    while (true) {      
+      if ((b.health -= a.damagePerAttack) <= 0) return a.name;  // a wins
+      if ((a.health -= b.damagePerAttack) <= 0) return b.name;  // b wins
+    }
+  
+  }
+}
