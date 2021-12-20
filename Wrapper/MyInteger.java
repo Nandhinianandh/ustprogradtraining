7d371a5418d6804880af0c3e4308e799070def32
Nandhini <53011385+Nandhinianandh@users.noreply.github.com>
2021-12-20 18:36:54 +0530

    Wrapper

---
+++ Wrapper/MyInteger.java
@@ -0,0 +1,11 @@
+public class MyInteger {
+    public Integer value;
+
+    MyInteger(Integer value){
+        this.value=value;
+
+    }
+    public String toString(){
+        return this.value.toString();
+    }
+}
