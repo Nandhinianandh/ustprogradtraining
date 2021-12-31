2745db86d74bc2d79f2cb3b1d1e2cfa74ac942e7
Nandhini <53011385+Nandhinianandh@users.noreply.github.com>
2021-12-31 10:32:02 +0530

    abcbank

---
+++ ABCBank/src/main/java/AbcBankApplication.java
@@ -0,0 +1,15 @@
+package com.abcBank.abcbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class AbcBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbcBankApplication.class, args);
+    }
+
+}
+
